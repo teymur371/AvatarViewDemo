6d81b4c5f5a3e7cbc04f483907e4e5dac7511591
Teymur Asadov <teymur371@gmail.com>
2021-03-08 10:31:20 +0200

    Add project files.

---
+++ AvatarViewDemo/AvatarViewDemo/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AvatarViewDemo.ViewModels;
+using AvatarViewDemo.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AvatarViewDemo
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
